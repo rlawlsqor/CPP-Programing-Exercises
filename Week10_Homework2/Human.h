cbde7495cc2e9d119c570229d1ac576ffeafa8cd
rlawlsqor <102772362+rlawlsqor@users.noreply.github.com>
2022-05-20 23:42:35 +0900

    Add files via upload

--- Week10_Homework2/Human.h
+++ Week10_Homework2/Human.h
@@ -5,7 +5,7 @@ enum Direction { Left = 75, Right = 77, Up = 72, Down = 80 };
 
 class Human : public Monster {
 public:
-	Human(string n = "ë¯¸ë˜ì¸ë¥˜", string i = "â™€", int px = 0, int py = 0)
+	Human(string n = "¹Ì·¡ÀÎ·ù", string i = "¡Ï", int px = 0, int py = 0)
 		: Monster(n, i, px, py) {}
 	~Human() { cout << " [Human   ]"; }
 	int getDirKey() { return getche() == 224 ? getche() : 0; }

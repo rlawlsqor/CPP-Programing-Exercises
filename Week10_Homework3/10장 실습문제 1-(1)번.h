c7c062febb91935688e1820c0b7d34f2b3fb4719
rlawlsqor <102772362+rlawlsqor@users.noreply.github.com>
2022-05-21 00:06:57 +0900

    Add files via upload

---
+++ Week10_Homework3/10장 실습문제 1-(1)번.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Monster.h"
+#include <conio.h>
+enum Direction { Left = 75, Right = 77, Up = 72, Down = 80 };
+
+class Human : public Monster {
+public:
+	Human(string n = "미래인류", string i = "♀", int px = 0, int py = 0)
+		: Monster(n, i, px, py) {}
+	~Human() { cout << " [Human   ]"; }
+	int getDirKey() { return getche() == 224 ? getche() : 0; }
+	void move(int** map, int maxx, int maxy) {
+		if (kbhit()) {
+			char ch = getDirKey();
+			if (ch == Left) x--;
+			else if (ch == Right) x++;
+			else if (ch == Up) y--;
+			else if (ch == Down) y++;
+			else return;
+			clip(maxx, maxy);
+			eat(map);
+		}
+	}
+};
+
+class Tuman : public Human {
+public:
+	Tuman(string n = "미래투맨", string i = "♀", int px = 0, int py = 0)
+		: Human(n, i, px, py) {}
+	~Tuman() { cout << " [Tuman   ]"; }
+	void move(int** map, int maxx, int maxy, char ch) {
+		if (ch == Left || ch == 'a') x--;
+		else if (ch == Right || ch == 'd') x++;
+		else if (ch == Up || ch == 'w') y--;
+		else if (ch == Down || ch == 's') y++;
+		else return;
+		clip(maxx, maxy);
+		eat(map);
+	}
+};
